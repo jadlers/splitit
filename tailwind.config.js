c265d474a6c4d9272099e711b940ecb26fd62255
Jacob Adlers <jacob.adlers@gmail.com>
2020-12-23 22:16:08 +0100

    Initialize project with craco and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
